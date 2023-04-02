187b55cc2b6552ecd1b182067f2897ac02347825
snpronyakin <spkobra2@gmail.com>
2023-04-02 16:27:42 +0300

    Refactoring class

--- test_add_new_group.py
+++ test_add_new_group.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
+
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
@@ -11,7 +12,7 @@ class UntitledTestCase(unittest.TestCase):
 
     def test_untitled_test_case(self):
         dw = self.wd
-        dw.get("http://localhost/addressbook/index.php")
+        dw.get("http://localhost/addressbook/")
         dw.find_element_by_name("user").clear()
         dw.find_element_by_name("user").send_keys("admin")
         dw.find_element_by_name("pass").click()
@@ -32,15 +33,19 @@ class UntitledTestCase(unittest.TestCase):
         dw.find_element_by_name("submit").click()
         dw.find_element_by_link_text("group page").click()
         dw.find_element_by_link_text("Logout").click()
-    
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to.alert()
+        except NoAlertPresentException:
+            return False
         return True
 
     def tearDown(self):
