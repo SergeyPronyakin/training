a2411b8e336ddf8831b9441e169e7ede2af71b0f
snpronyakin <spkobra2@gmail.com>
2023-04-09 20:57:04 +0300

    add 2 tests - edit account and dalete accounts

--- fixture/account_helper.py
+++ fixture/account_helper.py
@@ -27,3 +27,25 @@ class AccountHelper:
     def return_to_the_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
+    def delete_accounts(self):
+        wd = self.app.wd
+        count_of_accounts = len(wd.find_elements_by_xpath('//img[@alt="Edit"]'))
+        while count_of_accounts > 0:
+            wd.find_element_by_xpath('//img[@alt="Edit"]').click()
+            wd.find_element_by_xpath('//input[@value="Delete"]').click()
+            count_of_accounts -= 1
+
+        assert int(wd.find_element_by_id('search_count').text) == 0
+
+    def edit_account(self):
+        name = "Assert name"
+        wd = self.app.wd
+        wd.find_element_by_xpath('//img[@alt="Edit"]').click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(name)
+        wd.find_element_by_name("update").click()
+        self.return_to_the_home_page()
+
+        # Assert first name at home page after updating
+        assert wd.find_element_by_xpath("/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[3]").text == name
---
+++ tests/test_edit_account.py
@@ -0,0 +1,7 @@
+from model.account import UserData
+
+
+def test_delete_accounts(app):
+    app.session.login(UserData().username, UserData().password)
+    app.account_helper.edit_account()
+    app.session.logout()
