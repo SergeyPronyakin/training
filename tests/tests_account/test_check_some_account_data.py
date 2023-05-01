2754736b724eca8f0b7fc68f90ae5a3a50c1d0ac
snpronyakin <spkobra2@gmail.com>
2023-05-01 13:54:32 +0300

    task_14, v2

--- tests/tests_account/test_check_some_account_data.py
+++ tests/tests_account/test_check_some_account_data.py
@@ -3,16 +3,14 @@ from model.account import AccountData
 
 
 def test_check_some_account_data(app):
-    account_helper = app.account_helper
+    if not app.account_helper.count_of_accounts():
+        app.account_helper.create_account(AccountData())
 
-    if not account_helper.count_of_accounts():
-        account_helper.create_account(AccountData())
-
-    count_of_accounts = int(account_helper.get_accounts_count_from_page())
+    count_of_accounts = int(app.account_helper.get_accounts_count_from_page())
     random_account = random.randrange(count_of_accounts)
 
-    account_at_home_page = account_helper.accounts()[random_account]
-    account_at_edit_page = account_helper.get_account_data_from_edit_page_by_index(random_account)
+    account_at_home_page = app.account_helper.accounts()[random_account]
+    account_at_edit_page = app.account_helper.get_account_data_from_edit_page_by_index(random_account)
 
     all_phones_from_home_page = account_at_home_page.all_phones_from_home_page
     all_phones_from_edit_page = account_at_edit_page
@@ -21,10 +19,10 @@ def test_check_some_account_data(app):
     all_emails_from_edit_page = account_at_edit_page
 
     # Check phones
-    assert all_phones_from_home_page == account_helper.merge_phones_like_at_home_page(all_phones_from_edit_page)
+    assert all_phones_from_home_page == app.account_helper.merge_phones_like_at_home_page(all_phones_from_edit_page)
 
     # Check emails
-    assert all_emails_from_home_page == account_helper.merge_emails_like_at_home_page(all_emails_from_edit_page)
+    assert all_emails_from_home_page == app.account_helper.merge_emails_like_at_home_page(all_emails_from_edit_page)
 
     # Check names
     assert account_at_edit_page.firstname == account_at_home_page.firstname
